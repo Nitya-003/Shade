f5cb8bd37ee36b8fba8af0d3f371ebbe8de17979
Nitya Gosain <nityagosain@gmail.com>
2025-08-13 12:47:45 +0530

    Update Newsletter.tsx

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -20,7 +20,7 @@ const Newsletter = () => {
 
   return (
     <section className="py-20 relative overflow-hidden">
-      {/* Background gradients */}
+      { }
       <div className="absolute inset-0">
         <div className="absolute top-1/2 left-1/4 w-96 h-96 bg-purple-500/5 rounded-full blur-3xl"></div>
         <div className="absolute top-1/2 right-1/4 w-96 h-96 bg-cyan-500/5 rounded-full blur-3xl"></div>
